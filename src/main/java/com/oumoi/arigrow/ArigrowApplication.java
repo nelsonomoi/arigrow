4988e926c85b8518be3014d1e3d6ce0d5bb2f05b
Nelson Omoi <nelson.omoi@smart.local>
2023-08-03 21:46:50 +0300

    initial commit

---
+++ src/main/java/com/oumoi/arigrow/ArigrowApplication.java
@@ -0,0 +1,13 @@
+package com.oumoi.arigrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArigrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArigrowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oumoi/arigrow/ArigrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oumoi.arigrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArigrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
